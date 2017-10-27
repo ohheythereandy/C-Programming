7e8bdae0747692ed4c8a8a5e9f2d0db932b49b0f
Andy Rosas <andyjrosas@gmail.com>
2017-10-27 14:02:05 -0700

    Created main.cpp entry point for program and deleted default file that was created

---
+++ Base Change Assignment/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Andy Rosas on 10/27/17.
+//
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int givenNumber;
+    int base;
+    
+    cout << "Welcome!" << endl;
+    cout << "Please enter a number: " << endl;
+    cin >> givenNumber;
+    
+    cout << "Please enter a base to convert it to!" << cout;
+    cout << "2.Binary\n8.Octal\n16.Hexadecimal" << endl;
+    cin >> base;
+    
+    
+    
+    cout << "Thanks! see you later";
+    return 0;
+    
+}
